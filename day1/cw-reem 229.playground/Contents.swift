b1aec52ce4ebc719db021ad6cfe73496089c6ec6
Reeeem229 <66528681+Reeeem229@users.noreply.github.com>
2020-06-14 17:42:56 +0300

    done

---
+++ day1/cw-reem 229.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+var str = "Hello, playground"
+let name = " reem "
+var age = 16
+var hobby1 = " Draw "
+var hobby2 = " Singing "
+var height =  168
+print (" HI my name is \(name)and i like \(hobby1) and \(hobby2)")
+print ("Im \(age) and \(height) cm tall")
+
+var friendName =  "shihana"
+var friendHobby1 = "skating"
+var friendHobby2 = "draw"
+var friendAge = 16
+var friendHeight = 150
+
+var ageDifference = age - friendAge
+var ageCompare = 15 < 16
+var nameDifference = name != friendName
+var hobby1Difference = "draw = skating"
+var hobby2Difference = 168 > 1.5
+
+name == friendName
+name != friendName
+
+hobby1 == friendHobby1
+hobby1 != friendHobby1
+hobby2 == friendHobby2
+hobby2 != friendHobby2
+hobby1 == friendHobby2
+hobby1 != friendHobby2
+hobby2 == friendHobby1
+hobby2 != friendHobby1
+
+age > friendAge
+age < friendAge
+age >= friendAge
+age <= friendAge
+
+height == friendHeight
+height < friendHeight
+height > friendHeight
+height >= friendHeight
+height != friendHeight
--- day1/ios-hw-1.playground/Contents.swift
+++ day1/ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
+  
+var name2 = "Salem"
 
-name = "Salem"
 
-
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -31,28 +31,33 @@ var bmi = myWeight * (height * height)
  * رقم الهاتف
  * العمر
  * البلد
- * منطقة السكن
+ * منطقة الس
  * باسوورد
  * كويتي ؟
  
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "reem"
+var lastName1 = "alotaibi"
+var email1 = "reemalotaibi090@hotmail.com"
+var phoneNumber1 = 98849850
+var age1 = 16
+var country1 = "kuwait"
+var area1 = "kuwait"
+var password = "366"
 var isKuwaiti = false
 
 //Person 2
-
-
-
-
+var firstName2 = "shihana"
+var lastName2 = " mohammed"
+var email2 = "shihana090 @gmail .com "
+var phoneNumber = 765445679
+var age2  = 16
+var country2 = "kuwait"
+var  area2 = "mishrif "
+var Password2 = "877"
+var iskuwaiti = false
 
 
 
@@ -60,18 +65,20 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
-
-
-
-
-
-
+var firstName3 = "jood"
+var lastName3  = " alotaib'"
+var email3 = "reem090@gmail.com"
+var phoneNumber3 =  93436733
+var age3 = 17
+var country3 = "kuwait"
+var area3 = " ahmadi "
+var Password3 = "775"
+var iskuwait = false
 
 /*
 - - - -
-# المطلوب الثالث
+# المطلوب الثالث \\
+
 * قم بمقارنة معلومات كل شخص مع الشخص الاخر
  * قارن الاعمار اذا كانت متساوية ام لا
  * قارن منطقة السكن اذا كانت متساوية ام لا
@@ -79,6 +86,29 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
  
  */
+
+ 
+age1 == 16 && age2 == 16 && age3 == 17
+age1 == 16 && age2 != 16 &&
+area1 == "kuwait" && area2 == "mishrif" && area3 == "ahmadi"
+ age1 != 17 && age2 != 17 && age3 != 17
+age1 == 17 && age2 == 17 && age3 == 17
+age1 < 18 && age2 < 18 && age3 < 18
+ age1 > 18 && age2 > 18 && age3 > 18
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
